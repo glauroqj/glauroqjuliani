ac2fa0bcda3580cd2230cd24124d5804bf8e9624
glauro <glauro.juliani@hotmail.com>
2016-05-30 11:39:55 -0300

    Alterações no geral!

--- app/js/glauro.js
+++ app/js/glauro.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
 	
+	listar_sobre();
+
+	$('#sobre').click(function(event) {
+
+		listar_sobre();
+
+	});
+
+	function listar_sobre(){
+		$.ajax({
+			url: 'http://localhost/glauroqjuliani/app/content/texto.json',
+			type: 'GET',
+			dataType: 'json',
+			success: function(json){
+				$.each(json, function(index, val) {
+					var txt = (val.id)
+					console.log(txt)
+				});
+            },//success
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+            	alert('Erro!');
+    		}//error
+    	});
+	}
 
 });//end document
