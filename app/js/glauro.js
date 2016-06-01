8b88fa1c5f2b591c8cacafe13937de822f06a893
glauro <glauro.juliani@hotmail.com>
2016-06-01 18:24:42 -0300

    Alteração layout / ajax

--- app/js/glauro.js
+++ app/js/glauro.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {	
 
 	$( document ).ajaxComplete(function() {
-		$('.overlay').hide();
+			$('.overlay').hide();
 	});
 
 	$('.navbar-header button').click(function(event) {
-		$('#menu_glauro').toggleClass('collapse');
-
+		$('#menu_glauro').slideToggle(150);
 		$('.nav li').click(function(event) {
-			$('#menu_glauro').toggleClass('collapse');
+			$('#menu_glauro').slideUp(150);
 		});
 	});
 
 	$('#sobre').click(function(event) {
-
 		listar_sobre();
-
+	});
+	$('.logo').click(function(event) {
+		listar_sobre();
 	});
 
-
-
 });//end document
+
+//calls ajax
+
