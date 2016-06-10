fe6c6f55639379e67f1a8e91e60f7ff366e34b26
glauro <glauro.juliani@hotmail.com>
2016-06-10 17:33:23 -0300

    Alteração call ajax

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -1,16 +1,20 @@
 app.controller('SobreCtrl', function($rootScope, $location)
 {
 	$rootScope.aba = $location.path();
+	listar_sobre();
 });
 
 app.controller('EnsinoCtrl', function($rootScope, $location)
 {
 	$rootScope.aba = $location.path();
+	listar_ensino();
 });
 
 app.controller('PortfolioCtrl', function($rootScope, $location)
 {
 	$rootScope.aba = $location.path();
+	listar_portfolio();
+
 });
 
 app.controller('ContatoCtrl', function($rootScope, $location)
