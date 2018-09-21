a8bc2bbb33dc6dc85992d4d00735c7c6f117499f
glauroqj <glauro.juliani@hotmail.com>
2018-09-21 02:04:48 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -11,21 +11,3 @@ class App extends Component {
 }
 
 export default App
-
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <Switch>    
-//           <Route exact path="/" component={Home} />
-//           <Route exact path="/admin" component={Admin} />
-//           <Route path="/create" component={CreateAccount} />
-//           <Route path="/dashboard" component={Dashboard} />
-//           <Route path="/my-account" component={MyAccount} />
-//           <Route path="/forgot-password" component={ForgotPassword} />
-//           <Redirect to="/" />
-//       </Switch>
-//     );
-//   }
-// }
